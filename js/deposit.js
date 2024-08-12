1ef87d965206d696af1337bf4ce5f8e1e3173359
mahinhaider29 <mhdhdr29@gmail.com>
2024-08-12 19:13:03 +0600

    last commit

--- js/deposit.js
+++ js/deposit.js
@@ -1,25 +1,34 @@
 document.getElementById('deposit-btn').addEventListener('click', function(){
-    // new deposit amount : 
+    //1. new deposit amount : 
     const depositField = document.getElementById('deposit-field')
     var newDepositValueString = depositField.value;
     // console.log(typeof depositValue); -->  string ashe
     var newDepositValueNum = parseFloat(newDepositValueString);
 
-    // previous deposit total
+    //6. remove new deposit value
+    depositField.value = '';
+
+    // jdi new deposit value te string ba kichu na daoa hoy tokhon NaN hoye jay etar jonno condition daoao hoyeche NaN hole function bondho kora hobe
+    if(isNaN(newDepositValueNum)){
+        alert('Please enter a valid number')
+        return
+    }
+  
+    //2. previous deposit total
     const depositArea = document.getElementById('deposit-area')
     var previousDepositAmountString = depositArea.innerText;
     var previousDepositAmountNum = parseFloat(previousDepositAmountString);
     
-    //new deposit total
+    //3. new deposit total
     depositArea.innerText = previousDepositAmountNum + newDepositValueNum;
     
 
-    // previous total balance
+    //4.  previous total balance
     const balanceArea = document.getElementById('balance-area')
     var previousBalanceAmountString = balanceArea.innerText;
     var previousBalanceAmountNum = parseFloat(previousBalanceAmountString);
 
-     //new deposit total
+    //5. new deposit total
      balanceArea.innerText = previousBalanceAmountNum + newDepositValueNum;
      depositField.value = '';
 
